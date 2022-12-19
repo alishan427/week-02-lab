37a411272c9317938eb60aff15eb8f7672f76d32
Ali Shan <alishanamin427@gmail.com>
2022-12-19 19:15:49 +0500

    message

---
+++ cow.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+main() {
+cout << "   - - - - - - - - - - - - - - - - - - - - - -   " << endl << endl;
+
+cout << "             o         ^__^                        "  << endl;
+
+cout << "                 o    (o o)\\__________   " << endl << endl;
+
+
+cout << "                       (__) \\             )\\/\\  " << endl;
+
+cout << "                            ||_ _ _ _W |          " << endl << endl;
+
+cout << "                            ||        ||            " << endl;
+
+}
